8de3e0a14bd91d87b6a94f3e696e494303d87c23
Ramahafaly Edmond <ramahafaly2.0gmail.com>
2021-10-22 22:23:18 +0300

    change and add new plugin

--- nvim/lua/plugin_settings/vim_airline.lua
+++
@@ -1,8 +0,0 @@
--- #let g:airline#extensions#tabline#formatter = 'default'
--- vim.cmd("let g:airline#extensions#tabline#formatter = 'unique_tail'")
---
--- choose which path formatter airline uses
-vim.g["airline#extensions#tabline#formatter"] = 'unique_tail'
-
-vim.g["airline#extensions#tabline#enabled"] = 1
-
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,32 +5,36 @@ return require('packer').startup(function()
   -- LSP
   use 'neovim/nvim-lspconfig'
   use 'nvim-lua/completion-nvim'
-  
+
   use 'nanotee/nvim-lua-guide'
   use 'hrsh7th/nvim-compe'
 
+  use {
+   'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function() require'nvim-tree'.setup {} end
+  }
 
-  use 'kyazdani42/nvim-tree.lua'
-  use 'kyazdani42/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
 
   use 'tpope/vim-fugitive'
   use 'tpope/vim-commentary'
   use 'tpope/vim-surround'
-  
+
   use 'christoomey/vim-tmux-navigator'
   use 'mbbill/undotree'
- 
-  
-  -- fzf
+
+  -- fuzy file searcher with fzf
   use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
   use {'junegunn/fzf.vim'}
   use 'pbogut/fzf-mru.vim'
 
-  use 'morhetz/gruvbox'
-  -- use 'overcache/NeoSolarized'
-  -- fuzy file searcher with fzf
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
+  -- https://github.com/marko-cerovac/material.nvim/blob/main/README.md
+  use 'marko-cerovac/material.nvim'
+
   use 'vim-ruby/vim-ruby'
 
   use "lukas-reineke/indent-blankline.nvim"
